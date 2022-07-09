fbca7090590286ae3932faa869c256d771a4ae12
DionMontolalu <dionmontolalu@gmail.com>
2022-07-09 15:09:38 +0800

    contoh penggunaan form di vue

---
+++ form/index.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        title: "",
+        description: "",
+        price: "",
+        paket: '',
+        category: '',
+        gender: true,
+    },
+});
